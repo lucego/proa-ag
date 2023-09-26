6aad03d0633118c054cd35e4e7f4dd9d983297e1
Luis C. González <54751005+lucego@users.noreply.github.com>
2023-09-26 00:10:23 -0300

    Add files via upload

---
+++ Turismo-Web/database/php/unirlo.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "BDActividades"; // Nombre de base de datos
+
+$conn = new mysqli($servername, $dbUsername, $dbPassword, $dbName);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("La conexión a la base de datos ha fallado: " . $conn->connect_error);
+}
+
+// Recibir datos del formulario
+$newUsername = $_POST['newUsername'];
+$newEmail = $_POST['newEmail'];
+$newPassword = password_hash($_POST['newPassword'], PASSWORD_BCRYPT); // Hashear la contraseña antes de almacenarla en la base de datos
+
+// Insertar datos en la base de datos
+$sql = "INSERT INTO usuarios (username, email, password) VALUES (?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sss", $newUsername, $newEmail, $newPassword);
+
+if ($stmt->execute()) {
+    echo "Registro exitoso. ¡Bienvenido, $newUsername!";
+} else {
+    echo "Error al registrar: " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
+?>
